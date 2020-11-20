6489259107cf5b5052b314c7eeafcf2864610501
Vahagn Gevorgyan <vahagn.gevorgyan@picsart.com>
2020-11-21 01:36:39 +0400

    SettingsViewController is created.

---
+++ Weather/Controllers/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Weather
+//
+//  Created by Vahagn Gevorgyan on 21.11.20.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
